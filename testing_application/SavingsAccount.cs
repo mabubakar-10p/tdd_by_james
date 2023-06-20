6444d553f9cd2c27cd4a06df34c0fba5fa2fa72e
Muhammad Abubakar Bin Shahid <MAbubakarBinShahid@mobilehealth.com>
2023-06-20 16:10:49 +0500

    Add project files.

---
+++ testing_application/SavingsAccount.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testing_application
+{
+    public class SavingsAccount
+    {
+        private int balance = 0;
+
+        public void deposit(int amount)
+        {
+            balance += amount;
+        }
+
+        public int getBalance() { return balance; }
+
+        public void withdraw(int amount)
+        {
+            balance -= amount;
+        }
+
+        public SavingsAccount nextYear(int interestRate)
+        {
+            SavingsAccount result = new SavingsAccount();
+            int newBalance = getBalance() + (getBalance() * interestRate / 100);
+            result.deposit(newBalance);
+            return result;
+        }
+
+    }
+}
