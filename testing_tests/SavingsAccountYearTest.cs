4f0ad6dc5d0421c7de1e4adf4eeafeec48e69baf
Muhammad Abubakar Bin Shahid <MAbubakarBinShahid@mobilehealth.com>
2023-07-04 17:00:00 +0500

    Assert.Equals changed to Assert.AreEqual

--- testing_tests/SavingsAccountYearTest.cs
+++ testing_tests/SavingsAccountYearTest.cs
@@ -11,33 +11,33 @@ namespace testing_tests
         public void getStartingBalance()
         {
             SavingsAccountYear account = newAccount();
-            Assert.Equals(10000,account.getStartingBalance());
+            Assert.AreEqual(10000,account.getStartingBalance());
         }
 
         [TestMethod]
         public void getEndingBalanceMatchesConstructor()
         {
-            Assert.Equals(10000, newAccount().getEndingBalance());
+            Assert.AreEqual(10000, newAccount().getEndingBalance());
         }
 
         [TestMethod]
         public void getInterestRateMatchesConstructor()
         {
-            Assert.Equals(10, newAccount().getInterestRate());
+            Assert.AreEqual(10, newAccount().getInterestRate());
         }
 
         [TestMethod]
         public void nextYearsStartingBalanceEqualThisYearEndingBalance()
         {
             SavingsAccountYear thisYear = newAccount();
-            Assert.Equals(thisYear.getEndingBalance(), thisYear.nextYear().getStartingBalance());
+            Assert.AreEqual(thisYear.getEndingBalance(), thisYear.nextYear().getStartingBalance());
         }
 
         [TestMethod]
         public void nextYearsInterestRateEqualThisYearInterestRate()
         {
             SavingsAccountYear thisYear = newAccount();
-            Assert.Equals(thisYear.getInterestRate(), thisYear.nextYear().getInterestRate());
+            Assert.AreEqual(thisYear.getInterestRate(), thisYear.nextYear().getInterestRate());
         }
 
         [TestMethod]
@@ -45,7 +45,7 @@ namespace testing_tests
         {
             SavingsAccountYear year = newAccount();
             year.withdraw(1000);
-            Assert.Equals(9000,year.getEndingBalance());
+            Assert.AreEqual(9000,year.getEndingBalance());
         }
 
         [TestMethod]
@@ -53,9 +53,9 @@ namespace testing_tests
         {
             SavingsAccountYear year = new SavingsAccountYear(10000,7000,10);
             year.withdraw(3000);
-            Assert.Equals(7700, year.getEndingBalance());
+            Assert.AreEqual(7700, year.getEndingBalance());
             year.withdraw(5000);
-            Assert.Equals(2000+200-(1250), year.getEndingBalance());
+            Assert.AreEqual(2000+200-(1250), year.getEndingBalance());
         }
 
         private SavingsAccountYear newAccount()
